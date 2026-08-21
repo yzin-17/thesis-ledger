5610c617c1aba5b2b81f78fdc3d4718e60b039db
yzin <30586807+yzin-17@users.noreply.github.com>
2026-08-21 14:45:53 +0800

    refactor: register screenshot import services

--- apps/server/src/imports/import.module.ts
+++ apps/server/src/imports/import.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
 import { LedgerModule } from '../ledger/ledger.module.js';
+import { AssetMatcherService } from './asset-matcher.service.js';
+import { ImportCommitService } from './import-commit.service.js';
 import { ImportController } from './import.controller.js';
+import { ImportDraftService } from './import-draft.service.js';
+import { ImportRollbackService } from './import-rollback.service.js';
 import { ImportService } from './import.service.js';
 
 @Module({
   imports: [LedgerModule],
   controllers: [ImportController],
-  providers: [ImportService],
-  exports: [ImportService],
+  providers: [
+    AssetMatcherService,
+    ImportDraftService,
+    ImportCommitService,
+    ImportRollbackService,
+    ImportService,
+  ],
+  exports: [ImportService, ImportDraftService, ImportCommitService, ImportRollbackService],
 })
 export class ImportModule {}
