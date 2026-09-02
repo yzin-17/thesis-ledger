ef7ed1b5cea60eeed174004fb7d5fbd04de5f971
yzin <30586807+yzin-17@users.noreply.github.com>
2026-09-02 20:08:24 +0800

    refactor: register risk boundary services

--- apps/server/src/risk/risk.module.ts
+++ apps/server/src/risk/risk.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { NotificationsModule } from '../notifications/notifications.module.js';
+import { RiskContextService } from './risk-context.service.js';
 import { RiskController } from './risk.controller.js';
+import { RiskEventService } from './risk-event.service.js';
+import { RiskRuleService } from './risk-rule.service.js';
 import { RiskService } from './risk.service.js';
 
 @Module({
   imports: [NotificationsModule],
   controllers: [RiskController],
-  providers: [RiskService],
+  providers: [RiskRuleService, RiskContextService, RiskEventService, RiskService],
   exports: [RiskService],
 })
 export class RiskModule {}
